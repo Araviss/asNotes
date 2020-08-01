0a7269777633d2c090630f71348dc66a48ce343d
araviss <jzon.l.livingston@gmail.com>
2020-07-31 23:30:32 -0400

    adding save function

--- app/src/main/java/com/example/notesapp/Notes.java
+++ app/src/main/java/com/example/notesapp/Notes.java
@@ -3,12 +3,25 @@ package com.example.notesapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class Notes extends AppCompatActivity {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Set;
 
+public class Notes extends AppCompatActivity {
+    SharedPreferences setOfNotes;
+    EditText notepadEditText;
+    ArrayList<String> notesList = new ArrayList<>(); //Array list created to hold ListView previews
+    Set<String> notesSet;
+    public static final String Notes = "notesKey";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +32,31 @@ public class Notes extends AppCompatActivity {
 
         TextView notesTitleTextView = (TextView) findViewById(R.id.notesTitleTextView);
         notesTitleTextView.setText(notesTitle);
-        EditText notepadEditText = (EditText) findViewById(R.id.notesTakenEditText);
+
+        notepadEditText = (EditText) findViewById(R.id.notesTakenEditText);
+        ImageButton saveButton = (ImageButton) findViewById(R.id.saveButton);
+
+        setOfNotes = getSharedPreferences("notes", MODE_PRIVATE);
+
+        saveButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    public void onClick(View v) { ///this is going to read set into an array add
+                        String writtenNotes = notepadEditText.getText().toString();
+                        SharedPreferences.Editor editor = setOfNotes.edit();
+
+                        if(!notesList.isEmpty()){
+                            notesList.add(writtenNotes);
+
+
+                            editor.putStringSet("n", notesSet);
+                        }
+                        else{
+                            notesSet.add(writtenNotes);
+                            editor.putStringSet("n",notesSet);
+                }
+              }
+        });
+
 
 
 
